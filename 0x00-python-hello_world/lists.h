aa96bbeefa7b0d07dead09403ccaa76f1c587125
sathagesey33 <abdukadirmuhanad@gmail.com>
2023-08-19 03:28:56 -0700

    cyle

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,25 +1,26 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdlib.h>
 
 /**
-* struct lisintager_t - singly link lists
-* @s: intager
-* @next: points to the next node
-*/
-typedef struct lisintager_t
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
 {
-	int s;
-	struct lisintager_t *next;
-} lisintager_s;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
-size_t print_lisintager(const lisintager_t *h);
-lisintager_t *add_nodeint(lisintager_t **head, const int n);
-void free_lisintager(lisintager_t *head);
-int check_cycle(lisintager_t *list);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
-
-
- 
 
