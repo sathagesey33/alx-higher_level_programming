33ab9bbbeec358059edcb9f2092f4df3600ac314
sathagesey33 <abdukadirmuhanad@gmail.com>
2023-11-14 10:18:23 -0800

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      // Set width and height to undefined if invalid values are provided
-      this.width = undefined;
-      this.height = undefined;
-    } else {
+    if ((w > 0 && h > 0)) {
       this.width = w;
       this.height = h;
     }
