98bed9fbc5601d4a9ec34f9ee3405a491a61ad74
sathagesey33 <abdukadirmuhanad@gmail.com>
2024-01-04 02:13:19 -0800

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,20 +1,11 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-function readAndPrintFile(filePath) {
-    try {
-        const content = fs.readFileSync(filePath, 'utf-8');
-        console.log(content);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-    }
-}
-
-if (process.argv.length !== 3) {
-    console.log('Usage: ./script.js <file_path>');
-    process.exit(1);
-}
-
 const filePath = process.argv[2];
-readAndPrintFile(filePath);
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
